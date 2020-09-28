a0034901f2b47b0cd06f4a86af55c96b678d1f62
Nikhil-fang12 <70902817+Nikhil-fang12@users.noreply.github.com>
2020-09-28 19:39:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bouncy-ball
-bouncy ball
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,34 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var ground,ball;
+
+function setup() {
+  createCanvas(800,400);
+
+  var groundOption = {
+    isStatic: true
+  }
+  ground = Bodies.rectangle{400,390,800,20,groundOption};
+  World.add(world,ground);
+
+  var ballOption = {
+    restitution: 1.0
+  }
+  ball = Bodies.circle(200,100,20,ballOption);
+  World.add(world.ball);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  Engine.uptade(engine);
+
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,800,20);
+
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20,20);
+}
